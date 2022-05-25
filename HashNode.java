a874045eb9f6343e08d671b33f6df3d7bd5b9f28
Jake Christensen <83853752+JakeChristensen5@users.noreply.github.com>
2022-05-25 18:09:52 -0500

    Create HashNode.java

---
+++ HashNode.java
@@ -0,0 +1,54 @@
+// --== CS400 Project One File Header ==--
+// Name: Jacob (Jake) Christensen
+// CSL Username: jakec
+// Email: jrchristens2@wisc.edu
+// Lecture #: 004 @4:00pm
+// Notes to Grader:
+
+import com.sun.jdi.Value;
+
+/**
+ * Helper class to help with chaining and managing for collisions
+ */
+public class HashNode <KeyType, ValueType> {
+
+        //data fields
+        KeyType key;
+        ValueType value;
+        HashNode<KeyType, ValueType> next; //next node in the list
+        final int hashCode;
+
+        /**
+         * Constructor
+         */
+        public HashNode(KeyType key, ValueType value, int hashCode) {
+            this.hashCode = hashCode;
+            this.key = key;
+            this.value = value;
+        }
+
+        public KeyType getKey() {
+            return key;
+        }
+
+        public ValueType getValue() {
+            return value;
+        }
+        
+        public Object getNext(KeyType key, ValueType value) {
+            return next;
+        }
+
+        public Object setNext(KeyType key, ValueType value, HashNode newNext){
+            return this.next = newNext;
+        }
+
+
+        public ValueType makeValue(ValueType value) {
+            ValueType oValue = value;
+            this.value = value;
+            return oValue;
+        }
+
+}
+
